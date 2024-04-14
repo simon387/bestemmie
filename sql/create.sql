2bb6ae2bc8aedba903c8c7cefc45d15eea0d9ede
simone.celia <simone.celia@eng.it>
2024-04-14 14:57:06 +0200

    format

--- sql/create.sql
+++ sql/create.sql
@@ -9,7 +9,7 @@ USE
 DROP TABLE IF EXISTS bestemmie;
 CREATE TABLE bestemmie
 (
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    bestemmia   VARCHAR(1024) NOT NULL,
-    created TIMESTAMP    NOT NULL
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    bestemmia VARCHAR(1024) NOT NULL,
+    created   TIMESTAMP     NOT NULL
 );
